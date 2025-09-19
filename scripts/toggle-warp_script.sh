170a0413e8e967c28f31468adc30550556e2659f
Ashik <110383734+ashik-maybe@users.noreply.github.com>
2025-09-19 23:00:07 +0600

    Update toggle-warp_script.sh

--- scripts/toggle-warp_script.sh
+++ scripts/toggle-warp_script.sh
@@ -4,8 +4,8 @@
 
 if warp-cli status 2>/dev/null | grep -q "Status update: Connected"; then
     warp-cli disconnect >/dev/null
-    echo "🔸 Disconnected from WARP"
+    echo "○ Disconnected from Cloudflare WARP"
 else
     warp-cli connect >/dev/null
-    echo "🔹 Connected to WARP"
+    echo "● Connected to Cloudflare WARP"
 fi
