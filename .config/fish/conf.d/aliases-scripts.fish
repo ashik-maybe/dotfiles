a77f54c74bce493fad9fe4cee0a21b429f5eb542
Ashik <110383734+ashik-maybe@users.noreply.github.com>
2026-02-17 22:59:25 +0600

    Update aliases-scripts.fish

--- .config/fish/conf.d/aliases-scripts.fish
+++ .config/fish/conf.d/aliases-scripts.fish
@@ -6,10 +6,10 @@ alias yt="$HOME/scripts/yt-dlp.sh"      # YouTube downloader wrapper
 alias k="$HOME/scripts/archive.sh"      # Archive files with custom logic
 alias x="$HOME/scripts/unarchive.sh"    # Extract archives with custom logic
 
-alias webapp="$HOME/scripts/make-webapp.sh"    # Turn websites into apps
+# alias webapp="$HOME/scripts/make-webapp.sh"    # Turn websites into apps
 
-alias warp="$HOME/scripts/toggle-warp.sh"         # Toggle Cloudflare WARP VPN
-alias patchvm='sudo $HOME/scripts/patch-vm-network.sh' # Restore VM internet access when Docker breaks libvirt's bridge
+# alias warp="$HOME/scripts/toggle-warp.sh"         # Toggle Cloudflare WARP VPN
+# alias patchvm='sudo $HOME/scripts/patch-vm-network.sh' # Restore VM internet access when Docker breaks libvirt's bridge
 
 alias time-stat="$HOME/scripts/time-stat.sh"      # Show time left in day/week/month/year
 alias days-till="$HOME/scripts/days-till.sh"    # Show time left for a certain date
