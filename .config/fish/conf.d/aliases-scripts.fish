e7737265465ebbea55c9e0739fcedd6063da7217
Ashik <110383734+ashik-maybe@users.noreply.github.com>
2026-03-16 00:40:26 +0600

    Remove unused days-till script and corresponding alias from aliases-scripts.fish

--- .config/fish/conf.d/aliases-scripts.fish
+++ .config/fish/conf.d/aliases-scripts.fish
@@ -11,6 +11,5 @@ alias x="$HOME/scripts/unarchive.sh"    # Extract archives with custom logic
 # alias patchvm='sudo $HOME/scripts/patch-vm-network.sh' # Restore VM internet access when Docker breaks libvirt's bridge
 
 alias time-stat="$HOME/scripts/time-stat.sh"      # Show time left in day/week/month/year
-alias days-till="$HOME/scripts/days-till.sh"    # Show time left for a certain date
 alias vid-len="$HOME/scripts/video_speed_calc.sh" # Calculate video length by playback speed
 alias book-len="$HOME/scripts/document_length_calc.sh" # Calculate book completion time by pace
