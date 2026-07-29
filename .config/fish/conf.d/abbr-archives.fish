e5f47c3d956539a752b28c8c2cd54c28563ff87a
Ashik <110383734+ashik-maybe@users.noreply.github.com>
2026-07-29 18:55:29 +0600

    add: abbr for rar, 7z files

--- .config/fish/conf.d/abbr-archives.fish
+++ .config/fish/conf.d/abbr-archives.fish
@@ -4,7 +4,7 @@
 # ARCHIVE & COMPRESSION ABBREVIATIONS
 # Pattern: [Action][Format]
 # Action: c = Compress, x = Extract, l = List
-# Format: z = gz, j = bz2, x = xz, s = zst, zip = zip
+# Format: z = gz, j = bz2, x = xz, s = zst, zip = zip, r = rar, 7 = 7z
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
@@ -13,21 +13,28 @@
 abbr -a cz   'tar -czvf'        # Compress .tar.gz (Standard)
 abbr -a cx   'tar -cJvf'        # Compress .tar.xz (High compression)
 abbr -a cj   'tar -cjvf'        # Compress .tar.bz2
-abbr -a cs   'tar --zstd -cvf'  # Compress .tar.zst (Fast)
+abbr -a cs   'tar --zstd -cvf'  # Compress .tar.zst (Fast/Modern)
 abbr -a ct   'tar -cvf'         # Compress raw .tar
-abbr -a czip 'zip -r'           # Compress .zip
+abbr -a czip 'zip -r'           # Compress directory to .zip
+abbr -a c7   '7z a'             # Compress/Create .7z archive
 
 # ------------------------------------------------------------------------------
-# EXTRACTION (x + format / x for any tar)
+# EXTRACTION (x + format)
 # ------------------------------------------------------------------------------
-abbr -a xt   'tar -xvf'         # Extract ANY tar archive (.tar.gz, .tar.xz, .tar.zst, etc.)
+abbr -a xt   'tar -xvf'         # Extract ANY tar archive (auto-detects gz, xz, etc.)
 abbr -a xzip 'unzip'            # Extract .zip
-abbr -a xs   'zstd -d'          # Extract standalone .zst
+abbr -a xr   'unar'             # Extract .rar (using unar for robust support)
+abbr -a x7   '7z x'             # Extract .7z archive (preserving paths)
+
+# Standalone single-file compression/decompression
+abbr -a xzst 'zstd -d'          # Extract standalone .zst
 abbr -a xgz  'gunzip -k'        # Extract standalone .gz
 abbr -a xxz  'xz -dk'           # Extract standalone .xz
 
 # ------------------------------------------------------------------------------
 # LIST CONTENTS (l + format)
 # ------------------------------------------------------------------------------
-abbr -a ltar   'tar -tvf'         # List contents of ANY tar archive
+abbr -a ltar 'tar -tvf'         # List contents of ANY tar archive
 abbr -a lzip 'unzip -l'         # List contents of .zip archive
+abbr -a lr   'unar -t'          # List contents of .rar archive
+abbr -a l7   '7z l'             # List contents of .7z archive
